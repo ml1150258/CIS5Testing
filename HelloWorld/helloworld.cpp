f43afeba3f2a5a17703b64944f61ca2e9dac6e1a
ml1150258 <mlehrjava@gmail.com>
2024-12-26 15:17:41 -0800

    NewToday

--- HelloWorld/helloworld.cpp
+++ HelloWorld/helloworld.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main(int argv,char **argc){
-    cout<<"Hello World"<<endl;
+    cout<<"Hello World New"<<endl;
     int n;
     cin>>n;
     cout<<n<<endl;
