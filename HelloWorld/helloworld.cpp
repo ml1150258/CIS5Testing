c809feea812fbe885cf6a4d007d444821172ba95
ml1150258 <mlehrjava@gmail.com>
2024-12-27 16:56:59 -0800

    copilotfirst

--- HelloWorld/helloworld.cpp
+++ HelloWorld/helloworld.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
-using namespace std;
-int main(int argv,char **argc){
-    cout<<"Hello World New"<<endl;
+#include <limits>
+
+int main(int argc, char **argv) {
+    std::cout << "Hello World New" << std::endl;
+
     int n;
-    cin>>n;
-    cout<<n<<endl;
-    cin>>n;
-    cout<<n<<endl;
+    std::cout << "Please enter a number: ";
+    while (!(std::cin >> n)) {
+        std::cin.clear(); // clear the error flag
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // discard invalid input
+        std::cout << "Invalid input. Please enter a number: ";
+    }
+    std::cout << "You entered: " << n << std::endl;
+
+    std::cout << "Please enter another number: ";
+    while (!(std::cin >> n)) {
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout << "Invalid input. Please enter a number: ";
+    }
+    std::cout << "You entered: " << n << std::endl;
+
+    return 0;
 }
